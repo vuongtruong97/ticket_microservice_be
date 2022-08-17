02331f44377bb62cf9fc88a02a5529bf64e07587
vuong truong <vuongtruong@Vuong-Pc.localdomain>
2022-08-17 13:07:50 +0700

    test commit console

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -13,7 +13,7 @@ const start = async () => {
     if (!process.env.RABBIT_URI) {
         throw new Error('RABBIT_URI is not define')
     }
-
+    console.log('Test console')
     try {
         await mongoose.connect(process.env.MONGO_URI)
 
